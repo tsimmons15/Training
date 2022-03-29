27ed420b7ddd09a6c211dd11cdba9660c27f240f
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-03-29 11:27:13 -0400

    Adding playgrounds

---
+++ WeekOne/DayTwo/src/ArrayPlayground.java
@@ -0,0 +1,13 @@
+
+
+public class ArrayPlayground {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+
+        System.out.println(nums[0]);
+    }
+}
