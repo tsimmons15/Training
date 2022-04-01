24031710478cc3d69ac7a77686d9a6f9b104c752
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-04-01 10:33:11 -0400

    Adding in the practice problems we've been working on.

---
+++ Weeks/WeekOne/Practice/src/main/java/Practice.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Practice {
+
+    public static void main(String [] args) {
+        EvaluationService es = new EvaluationService();
+
+        System.out.println(es.calculatePrimeFactorsOf(2L));
+    }
+
+}
