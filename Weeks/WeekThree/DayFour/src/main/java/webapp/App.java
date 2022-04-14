edb644d80fc96373355f611078a30aa7a095619d
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-04-14 10:55:29 -0400

    Javalin, pt. 1

--- Weeks/WeekThree/DayFour/src/main/java/webapp/App.java
+++ Weeks/WeekThree/DayFour/src/main/java/webapp/App.java
@@ -1,4 +1,13 @@
 package webapp;
 
+import io.javalin.Javalin;
+
 public class App {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7070);
+        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/fail", ctx -> ctx.result("No, you fail!"));
+        app.get("/hello/{name}", ctx -> ctx.result("Hello to you too, " + ctx.pathParam("name")));
+        app.get("/hello", ctx->ctx.result("Hello there, stranger!"));
+    }
 }
