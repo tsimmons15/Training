e16a3fe844813f8bf8154e3a5f03b02a3f207b0e
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-04-19 13:19:05 -0400

    Testing subrepos

--- Weeks/WeekFour/MathApp/src/main/java/dev/simmons/app/App.java
+++ Weeks/WeekFour/MathApp/src/main/java/dev/simmons/app/App.java
@@ -6,11 +6,11 @@ public class App {
     public static void main(String[] args) {
         Javalin app = Javalin.create();
 
-        app.get("/add/{num1}/{num2}", (ctx) -> {
-            int num1 = Integer.parseInt(ctx.pathParam("num1"));
-            int num2 = Integer.parseInt(ctx.pathParam("num2"));
-            System.out.println("Received: " + num1 + " " + num2);
-            ctx.result(""+(num1+num2));
+        app.get("/add/{num1}/{num2}", context -> {
+            int num1 = Integer.parseInt(context.pathParam("num1"));
+            int num2 = Integer.parseInt(context.pathParam("num2"));
+            System.out.println("Received: " + num1 + " + " + num2);
+            context.result("The sum is " + (num1+num2));
         });
 
         app.start(7070);
